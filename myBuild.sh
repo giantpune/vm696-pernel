49d5e1eb7ee3d1a4778a9bf8cabe452c4483c296
jenna <giantpune@gmail.com>
2012-10-01 00:59:44 -0400

    * build script uses /tmp

--- myBuild.sh
+++ myBuild.sh
@@ -22,19 +22,19 @@ CC1=${KERNEL_SDK}/linux-x86/toolchain/arm-eabi-4.4.3/prebuilt/linux-x86/libexec/
 export PATH=${KS}:${CC}:${CC1}:$PATH
 export TARGET_PRODUCT=m3s_virgin_us
 
-mkdir -p out
-make lge_m3s-perf_defconfig ARCH=arm CROSS_COMPILE=${CC}/arm-eabi- O=out -j2
-if [ "$?" -ne "0" ]; then
-  echo "make defconfig failed"
-  exit 1
-fi
+#mkdir -p out
+#make lge_m3s-perf_defconfig ARCH=arm CROSS_COMPILE=${CC}/arm-eabi- O=out -j2
+#if [ "$?" -ne "0" ]; then
+#  echo "make defconfig failed"
+#  exit 1
+#fi
 
 # build it
-make ARCH=arm CROSS_COMPILE=${CC}/arm-eabi- O=out -j2
-if [ "$?" -ne "0" ]; then
-  echo "Build failed"
-  exit 1
-fi
+#make ARCH=arm CROSS_COMPILE=${CC}/arm-eabi- O=out -j2
+#if [ "$?" -ne "0" ]; then
+#  echo "Build failed"
+#  exit 1
+#fi
 
 # check for abootimg
 if [ -z `which abootimg` ]; then
@@ -48,8 +48,19 @@ IMG_OUT="boot_"`date +%m_%d_%H_%M`".img"
 cp "vm696_zv5_stock_boot.img" ${IMG_OUT} || exit 1
 abootimg -u ${IMG_OUT} -k "out/arch/arm/boot/zImage" || exit 1
 
+# create zip archive file structure in temp folder (ramdisk)
+TMP_DIR=`mktemp -d`
+if [ "$?" -ne "0" ]; then
+  echo "Failed to make temp directory"
+  exit 1
+fi
+
+ORIG_DIR=`pwd`
+
+mkdir -p "${TMP_DIR}/META-INF/com/google/android"
+
 # write update-script 
-cat << EOF > "androidInstallableZip/META-INF/com/google/android/updater-script"
+cat << EOF > "${TMP_DIR}/META-INF/com/google/android/updater-script"
 assert( getprop("ro.product.device") == "m3s_virgin_us" );
 ui_print("");
 ui_print( "Installing `date +%m/%d/%Y_%H:%M` custom giantpune boot.img ..." );
@@ -57,18 +68,21 @@ show_progress(1.0,0);
 set_progress(0.33);
 package_extract_file( "${IMG_OUT}", "/dev/block/mmcblk0p9" );
 set_progress(0.66);
-set_progress(1.00);
 ui_print("Done !");
 EOF
 
-# copy boot.img into the zip folder
-cp "${IMG_OUT}" "androidInstallableZip/${IMG_OUT}" || exit 1
+# link boot.img and update binary into the zip folder.  zip will follow links
+ln -s "${ORIG_DIR}/${IMG_OUT}" "${TMP_DIR}/${IMG_OUT}" || exit 1
+ln -s "${ORIG_DIR}/update-binary" "${TMP_DIR}/META-INF/com/google/android/update-binary" || exit 1
 
 # zip it up
 ZIP_OUT="lgoe_pernel_"`date +%m_%d_%H_%M`".zip"
-cd androidInstallableZip
-zip -r ../"${ZIP_OUT}" *
-cd ..
-rm "androidInstallableZip/${IMG_OUT}"
-rm "androidInstallableZip/META-INF/com/google/android/updater-script"
+cd "${TMP_DIR}"
+zip -0 -r "${ORIG_DIR}/${ZIP_OUT}" *
+cd "${ORIG_DIR}"
+
+# remove temp directory
+rm -rf "${TMP_DIR}"
+
+# done
 echo "Installable zip created: "${ZIP_OUT}
